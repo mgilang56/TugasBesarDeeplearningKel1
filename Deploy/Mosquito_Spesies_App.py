18741979d657db595f27b27e93d88f04455cbc4a
M.Gilang Martiansyah M <114335151+mgilang56@users.noreply.github.com>
2024-12-16 15:39:59 +0700

    Update Mosquito_Spesies_App.py

--- Deploy/Mosquito_Spesies_App.py
+++ Deploy/Mosquito_Spesies_App.py
@@ -125,7 +125,7 @@ def add_bg_from_url():
         """
         <style>
         .stApp {
-            background-image: url('https://jenis.net/wp-content/uploads/2020/06/jenis-nyamuk-e1591437296119-768x456.jpg');
+            background-image: url("https://github.com/mgilang56/TugasBesarDeeplearningKel1/blob/main/Deploy/backgroundcoba1.jpg?raw=true");
             background-size: cover;
             background-position: top center;
             color: white;
